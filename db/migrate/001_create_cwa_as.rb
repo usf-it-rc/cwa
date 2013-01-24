7ec4d67e73f778eabd11485fdc7f07e9010e0879
Brian Smith <brs@usf.edu>
2013-01-23 20:08:30 -0500

    Remove un-needed migration

--- db/migrate/001_create_cwa_as.rb
+++
@@ -1,9 +0,0 @@
-class CreateCwaAs < ActiveRecord::Migration
-  def change
-    create_table :cwa_as do |t|
-      t.text :tos, :default => "Put your terms of service here"
-      t.text :saa, :default => "Put your Service Access Agreement here"
-      t.text :delete_saa, :default => "Put your terms of service upon account delete here"
-    end
-  end
-end
