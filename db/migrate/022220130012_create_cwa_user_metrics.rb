1adc0f0f64d4e82260a83a24ce66806433b119ea
Brian Smith <brs@usf.edu>
2013-04-15 15:18:35 -0400

    Fix names for cwa_user_metrics model

--- db/migrate/022220130012_create_cwa_user_metrics.rb
+++ db/migrate/022220130012_create_cwa_user_metrics.rb
@@ -2,13 +2,13 @@ class CreateCwaUserMetrics < ActiveRecord::Migration
   def change
     create_table :cwa_user_metrics do |t|
       t.integer :user_id
-      t.integer :cpu_time
-      t.float :cpu_time_job, :length => 22, :precision => 16
-      t.integer :job_time
+      t.integer :total_cputime
+      t.float :average_cputime, :length => 22, :precision => 16
+      t.integer :total_walltime
+      t.float :average_walltime, :length => 22, :precision => 16
       t.float :disk_usage_home, :length => 22, :precision => 16
       t.float :disk_usage_work, :length => 22, :precision => 16
-      t.integer :tot_jobs
-      t.float :average_job_time, :length => 22, :precision => 16
+      t.integer :total_jobs
     end
   end
 end
