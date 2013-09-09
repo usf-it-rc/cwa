5e062c2f01faae1c1d97c1e12bae21ac220a4d30
Brian Lindblom <brs@usf.edu>
2013-09-09 15:38:45 -0400

    Clean up debugging logs and some cruft

--- lib/thread_tracker.rb
+++ lib/thread_tracker.rb
@@ -2,6 +2,7 @@ class ThreadTracker < Thread
 
   attr_accessor :tid, :opid, :thread, :pool, :user, :timeout, :expired
   @@object_pool = []
+  @@lock = Mutex.new
   
   def initialize(&block)
 
@@ -23,12 +24,9 @@ class ThreadTracker < Thread
     # add cache entry item
     Rails.cache.write(self.opid, self.status)
 
-    Rails.logger.debug "#{user} kicking off thread #{tid} #{opid}"
-
     # kick off actual background task
     self.thread = super do
       Thread.current.thread_variable_set(:opid, self.opid)
-      Rails.logger.debug "#{user}:#{opid}:#{block.to_s}"
       begin
         yield block
       ensure
@@ -38,7 +36,9 @@ class ThreadTracker < Thread
     end
 
     # Still need this for find_thread_by_opid to work
-    @@object_pool << self
+    @@lock.synchronize do
+      @@object_pool << self
+    end
     self
   end  
 
@@ -48,8 +48,6 @@ class ThreadTracker < Thread
     t_list = Rails.cache.read("#{User.current.login}_thread_list")
     t_list[self.opid] = Time.now + 30.seconds
     Rails.cache.write("#{User.current.login}_thread_list", t_list)
-    Rails.logger.debug "ThreadTracker.expire() #{t_list.to_s}"
-    Rails.logger.debug "Removing thread #{self.opid}"
     self.expired = true
     self.kill
   end
@@ -60,11 +58,7 @@ class ThreadTracker < Thread
 
   def status=(stat)
     cur_stat = Rails.cache.read(self.opid)
-    if cur_stat.nil?
-      Rails.cache.write(self.opid, stat)
-    else
-      Rails.cache.write(self.opid, cur_stat.merge(stat))
-    end
+    Rails.cache.write(self.opid, cur_stat.nil? ? stat : cur_stat.merge(stat))
   end
 
   def status
@@ -75,53 +69,36 @@ class ThreadTracker < Thread
   class << self
     # clean up old threads
     def gc
-      Rails.logger.debug "ThreadTracker Garbage Collector"
-      @@object_pool.each do |t| 
-        t.expire if !t.alive?
+      @@lock.synchronize do
+        @@object_pool.each { |t| t.expire if !t.alive? }
       end
       
       # delete any old opids from the opid list
       t_list = Rails.cache.read("#{User.current.login}_thread_list")
       if !t_list.nil?
-        t_list.keys.each do |o|
-          t_list.delete(o) if !t_list[o].nil? and t_list[o] <= Time.now 
-        end
+        t_list.keys.each { |o| t_list.delete(o) if !t_list[o].nil? and t_list[o] <= Time.now }
         Rails.cache.write("#{User.current.login}_thread_list", t_list)
       end
-        
     end
 
     def my_ops
       t_list = Rails.cache.read("#{User.current.login}_thread_list")
-      return nil if t_list.nil?
-      t_list.keys
+      return t_list.nil? ? nil : t_list.keys
     end 
 
     def current
       opid = Thread.current.thread_variable_get(:opid)
-      if opid.nil?
-        Rails.logger.debug "We're not in a thread!"
-        return nil
-      else
-        t = find_thread_by_opid opid
-        Rails.logger.debug "ThreadTracker.current returning #{t.to_s}"
-        return t
-      end
+      return opid.nil? ? nil : find_thread_by_opid(opid)
     end
 
     def cache_status(opid)
-      resp = { opid => Rails.cache.read(opid) }
-      Rails.logger.debug "ThreadTracker.cache_status() => #{resp.to_s}"
-      resp
+      return { opid => Rails.cache.read(opid) }
     end
 
     def find_thread_by_opid(opid)
-      Rails.logger.debug "find_thread(#{opid})"
-      @@object_pool.each do |t| 
-        Rails.logger.debug "find_thread(#{t.to_s})"
-        return t if t.opid == opid
+      @@lock.synchronize do
+        return @@object_pool.select {|t| t.opid == opid }.first
       end
-
     end
   end
 
