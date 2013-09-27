ccd2bcd251d96e9e9208f406c16928edff1b9f02
Brian Lindblom <brs@usf.edu>
2013-09-27 13:06:32 -0400

    Ok, check for blank string instead

--- app/controllers/cwa_jobmanager_controller.rb
+++ app/controllers/cwa_jobmanager_controller.rb
@@ -75,7 +75,7 @@ class CwaJobmanagerController < ApplicationController
       @job.job_name = params[:job_name]
     end
 
-    if params.has_key?(:param_file) and !params[:param_file].nil?
+    if params.has_key?(:param_file) and params[:param_file] != ""
       # read the parameterization file, get column and row lengths
       param_data = []
       job_params = Redmine::CwaBrowserHelper.paramfile_parser(params[:param_file])
